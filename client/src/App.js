f300e5de0cfa90accbba39948f3ee06ce2d294a3
Bob Shens <kiranmanikonda123@gmail.com>
2024-06-17 16:14:01 -0700

    Setup Initial Scene

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+
+import { useState, useEffect } from 'react';
+
+import Scene from './components/Scene';
+
 import './App.css';
 
+
 function App() {
+
   return (
     <div>
-      heyo
+      <Scene/>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,14 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <App />
+</React.StrictMode>
+);
+
+/*
+  Strict mode renders scene twice:
+
+  <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+*/
