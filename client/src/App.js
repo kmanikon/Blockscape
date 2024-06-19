78217a6d0c808f44b43ef7f71467872db6f7b62b
Bob Shens <kiranmanikonda123@gmail.com>
2024-06-19 00:06:05 -0700

    Added Initial Game Board

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import * as THREE from 'three';
 import { createCamera } from './components/createCamera';
+import { createCity } from './components/city';
 import './App.css';
 
 function App() {
@@ -29,36 +30,98 @@ function App() {
       refContainer.current.appendChild(renderer.domElement);
     }
 
+    /*
     const geometry = new THREE.BoxGeometry(1, 1, 1);
     //const material = new THREE.MeshBasicMaterial({ color: 0x0afdcd });
     const material = new THREE.MeshLambertMaterial();
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
+    */
+
+    let terrain = [];
+    let buildings = [];
+
+    function initialize(city) {
+        scene.clear();
+        terrain = [];
+        buildings = [];
+        for (let x = 0; x < city.size; x++) {
+          const column = [];
+          for (let y = 0; y < city.size; y++) {
+            // Grass geometry
+            const geometry = new THREE.BoxGeometry(1, 1, 1);
+            const material = new THREE.MeshLambertMaterial({ color: 0x00aa00 });
+            const mesh = new THREE.Mesh(geometry, material);
+            mesh.position.set(x, -0.5, y);
+            scene.add(mesh);
+            column.push(mesh);
+          }
+          terrain.push(column);
+          buildings.push([...Array(city.size)]);
+        }
+
+        setupLights();
+    }
+
+    function update(city) {
+        for (let x = 0; x < city.size; x++) {
+          for (let y = 0; y < city.size; y++) {
+            // Building geometry
+            const tile = city.data[x][y];
+            if (tile.building && tile.building.startsWith('building')) {
+              const height = Number(tile.building.slice(-1));
+              const buildingGeometry = new THREE.BoxGeometry(1, height, 1);
+              const buildingMaterial = new THREE.MeshLambertMaterial({ color: 0x777777 });
+              const buildingMesh = new THREE.Mesh(buildingGeometry, buildingMaterial);
+              buildingMesh.position.set(x, height / 2, y);
+
+              if (buildings[x][y]) {
+                scene.remove(buildings[x][y]);
+              }
+
+              scene.add(buildingMesh);
+              buildings[x][y] = buildingMesh
+            }
+          }
+        }
+    }
 
     setupLights();
 
     function setupLights() {
-      const lights = [
-        new THREE.AmbientLight(0xffffff, 0.2),
-        new THREE.DirectionalLight(0xffffff, 0.3),
-        new THREE.DirectionalLight(0xffffff, 0.3),
-        new THREE.DirectionalLight(0xffffff, 0.3)
-      ];
+        const lights = [
+          new THREE.AmbientLight(0xffffff, 0.2),
+          new THREE.DirectionalLight(0xffffff, 0.3),
+          new THREE.DirectionalLight(0xffffff, 0.3),
+          new THREE.DirectionalLight(0xffffff, 0.3)
+        ];
 
-      lights[1].position.set(0, 1, 0);
-      lights[2].position.set(1, 1, 0);
-      lights[3].position.set(0, 1, 1);
+        lights[1].position.set(0, 1, 0);
+        lights[2].position.set(1, 1, 0);
+        lights[3].position.set(0, 1, 1);
 
-      scene.add(...lights);
+        scene.add(...lights);
     }
 
     const animate = () => {
-      requestAnimationFrame(animate);
-      mesh.rotation.x += 0.0;
-      mesh.rotation.y += 0.0;
+      //requestAnimationFrame(animate);
+      //mesh.rotation.x += 0.0;
+      //mesh.rotation.y += 0.0;
       renderer.render(scene, camera);
     };
+
+    function start() {
+      renderer.setAnimationLoop(animate);
+    }
+
     animate();
+
+
+
+    const city = createCity(16);
+
+    initialize(city);
+
 
     // Event listeners for camera controls
     window.addEventListener('mousedown', Camera.onMouseDown);
@@ -66,6 +129,20 @@ function App() {
     window.addEventListener('mousemove', Camera.onMouseMove);
 
     window.addEventListener('wheel', Camera.onMouseWheel);
+
+
+    const game = {
+      update() {
+        city.update();
+        update(city);
+      }
+    }
+
+    setInterval(() => {
+      game.update();
+    }, 1000)
+
+    start();
 
     // Cleanup on component unmount
     return () => {
