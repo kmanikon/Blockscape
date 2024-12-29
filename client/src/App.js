7719bc6c8fdf192ae2f131ac0906a9f78b6dbea0
Bob Shens <kiranmanikonda123@gmail.com>
2024-12-29 00:27:08 -0800

    Updated render target position

--- client/src/App.js
+++ client/src/App.js
@@ -211,7 +211,7 @@ function App() {
 
 
 
-      <div id="render-target" ref={refContainer} style={{ width: 'calc(100vw)', height: 'calc(100vh)', float: 'right', zIndex: -1}}>
+      <div id="render-target" ref={refContainer} style={{ position: 'absolute', width: 'calc(100vw)', height: 'calc(100vh)', float: 'right', zIndex: -1}}>
       </div>
     </>
   );
