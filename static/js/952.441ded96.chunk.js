6ca941e0e9ab9b9ca47f11ec20cc37ab94b6f166
kmanikon <103885541+kmanikon@users.noreply.github.com>
2025-09-23 19:35:40 -0500

    Updates

---
+++ static/js/952.441ded96.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksim_game=self.webpackChunksim_game||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.441ded96.chunk.js.map
